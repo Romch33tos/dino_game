28fc731a392f08faa2ded51ae6536df09c505696
Romch33tos <Romch33tos@yandex.ru>
2025-04-20 11:55:07 +0700

    Возможность перезапуска игры

--- Dino.py
+++ Dino.py
@@ -6,10 +6,10 @@ speed = -5
 class DinoGame:
     def __init__(self, master):
         self.master = master
-        self.master.title("Dino Game")
+        self.master.title("Динозаврик")
         self.master.geometry("600x600")
-        
-        self.score_label = tk.Label(master, text="Score: 0")
+        self.master.resizable(False,False)       
+        self.score_label = tk.Label(master, text="Счёт: 0")
         self.score_label.pack(pady = 5, anchor = "e", padx = 5)
         
         self.canvas = tk.Canvas(master, bg="white", width = 600, height=400)
@@ -18,14 +18,14 @@ class DinoGame:
         self.dino = self.canvas.create_rectangle(50, 300, 100, 350, fill="green")
         self.obstacle = None
         self.velocity = 0
-        self.gravity = 1
+        self.gravity = 0.7
         self.jumping = False
         self.score = 0
         self.is_game_over = False
 
-        self.jump_button = tk.Button(master, text="Jump", command=self.jump, width = 16, height = 2)
+        self.jump_button = tk.Button(master, text = "Прыжок!", command = self.jump, width = 16, height = 2)
         self.jump_button.pack(pady = 15)
-        
+
         self.canvas.bind("<KeyPress-Up>", self.jump)
         self.canvas.focus_set()
 
@@ -44,25 +44,28 @@ class DinoGame:
 
     def update_game(self):
         global speed
-        if not self.is_game_over:
+        if not self.is_game_over:     
             if self.jumping:
                 self.canvas.move(self.dino, 0, self.velocity)
                 self.velocity += self.gravity
                 if self.canvas.coords(self.dino)[1] >= 300:
                     self.canvas.move(self.dino, 0, 300 - self.canvas.coords(self.dino)[1])
-                    self.jumping = False
-                        
+                    self.jumping = False           
+       
             self.canvas.move(self.obstacle, speed, 0)
             obstacle_coords = self.canvas.coords(self.obstacle)
 
+            
             if self.check_collision():
                 self.is_game_over = True
-                self.canvas.create_text(400, 200, text="Game Over!", font=("Arial", 24), fill="red")
-            else:   
+                self.canvas.create_text(300, 200, text="Конец игры!", font=("Arial", 12), fill="red")
+                self.jump_button.configure(text = "Ещё раз?", command = self.restart_game)
+            else:
+               
                 if obstacle_coords[0] < 0:
                     self.canvas.delete(self.obstacle)
                     self.score += 1
-                    self.score_label.config(text=f"Score: {self.score}")
+                    self.score_label.config(text = f"Счёт: {self.score}")
                     speed -= 0.5
                     self.create_obstacle()
 
@@ -78,11 +81,14 @@ class DinoGame:
         return False
 
     def restart_game(self):
+        global speed
+        speed = -5
         self.canvas.delete("all")
         self.create_obstacle()
         self.dino = self.canvas.create_rectangle(50, 300, 100, 350, fill="green")
+        self.jump_button.configure(text = "Прыжок!", command = self.jump)
         self.score = 0
-        self.score_label.config(text="Score: 0")
+        self.score_label.config(text="Счёт: 0")
         self.is_game_over = False
         self.update_game()
 
