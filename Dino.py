31ca85a7b49a249a23f6edf474fd2db87dae226d
Romch33tos <Romch33tos@yandex.ru>
2025-04-22 08:48:02 +0700

    Реализация препятствий

--- Dino.py
+++ Dino.py
@@ -4,7 +4,9 @@ from tkinter import *
 from PIL import Image
 import shutil
 
-speed = -5
+list = ["Python/Программы/Игры/Dino/game_files/cacti2.png", "Python/Программы/Игры/Dino/game_files/cacti3.png", "Python/Программы/Игры/Dino/game_files/cacti4.png"]
+
+speed = -6.5
 
 shutil.unpack_archive("Python/Программы/Игры/Dino/game_files.zip", "Python/Программы/Игры/Dino/game_files")
 
@@ -21,9 +23,12 @@ class DinoGame:
         self.score_label = tk.Label(master, text="Счёт: 0")
         self.score_label.pack(pady=5, anchor="e", padx=5)
         root.protocol("WM_DELETE_WINDOW", delete)
+
         self.dinoPic = PhotoImage(file="Python/Программы/Игры/Dino/game_files/dino.png")
+        
         self.canvas = tk.Canvas(master, bg="white", width=600, height=400)
         self.canvas.pack(pady=5, padx=5)
+        
         self.dino = self.canvas.create_image(100, 300, image=self.dinoPic)
         self.obstacle = None
         self.velocity = 0
@@ -31,21 +36,24 @@ class DinoGame:
         self.jumping = False
         self.score = 0
         self.is_game_over = False
+
         self.jump_button = tk.Button(master, text="Прыжок!", command=self.jump, width=16, height=2)
         self.jump_button.pack(pady=15)
+
         self.canvas.bind("<KeyPress-Up>", self.jump)
         self.canvas.focus_set()
+
         self.create_obstacle()
         self.update_game()
 
     def create_obstacle(self):
-        self.cactiPic = PhotoImage(file="Python/Программы/Игры/Dino/game_files/cacti.png")
+        self.cactiPic = PhotoImage(file= random.choice(list))
         self.obstacle = self.canvas.create_image(800, 300, image=self.cactiPic)
 
     def jump(self, event=None):
         if not self.jumping and not self.is_game_over:
             self.jumping = True
-            self.velocity = -17
+            self.velocity = -18.5
 
     def update_game(self):
         global speed
@@ -58,6 +66,7 @@ class DinoGame:
                     self.jumping = False
             self.canvas.move(self.obstacle, speed, 0)
             obstacle_coords = self.canvas.coords(self.obstacle)
+
             if self.check_collision():
                 self.is_game_over = True
                 self.canvas.create_text(300, 200, text="Конец игры!", font=("Arial", 12), fill="red")
@@ -74,14 +83,15 @@ class DinoGame:
     def check_collision(self):
         dino_coords = self.canvas.coords(self.dino)
         obstacle_coords = self.canvas.coords(self.obstacle)
-        if (dino_coords[0] > obstacle_coords[0] and dino_coords[0] < obstacle_coords[2] and
-            dino_coords[1] > obstacle_coords[1]):
+        if (dino_coords[0] < obstacle_coords[0] + self.cactiPic.width() and
+            dino_coords[0] + self.dinoPic.width() > obstacle_coords[0] and
+            dino_coords[1] + self.dinoPic.height() > obstacle_coords[1]):
             return True
         return False
 
     def restart_game(self):
         global speed
-        speed = -5
+        speed = -6.5
         self.canvas.delete("all")
         self.create_obstacle()
         self.dino = self.canvas.create_image(100, 300, image=self.dinoPic)
@@ -94,4 +104,4 @@ class DinoGame:
 if __name__ == "__main__":
     root = tk.Tk()
     game = DinoGame(root)
-    root.mainloop()
+    root.mainloop() 
